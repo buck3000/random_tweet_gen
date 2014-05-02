b5d89d7eb1b3b692c75632050c97475b47445b8b
Howard O'Leary <howardeoleary@gmail.com>
2014-05-02 12:05:37 -0400

    edited image heights and widths

--- public/js/application.js
+++ public/js/application.js
@@ -6,14 +6,14 @@ $(document).ready(function() {
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 
  $('body').click(function(){
- 		var random = Math.floor(Math.random() * 13) + 1;
- 		$(".img").css("background", "url(../images/j-smith"+random+".jpg) no-repeat")
 	 	$.ajax({
 	 		type: "POST",
 	 		url: '/',
 	 		dataType: 'json'	
 	 	}).done(function(data){
 	 		$("h2").html(data);
+	 		var random = Math.floor(Math.random() * 12) + 1;
+ 			$(".img").css("background", "url(../images/j-smith"+random+".jpg) no-repeat")
 	 	})
 	 }); 
 });
